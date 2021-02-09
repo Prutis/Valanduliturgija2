a070df1159ff6bef1015f0c874fa16a5e1807c36
Rutenis Piksrys <rutenis@gmail.com>
2021-02-09 15:47:03 +0200

    Initial Commit

---
+++ Valandu liturgija/Valandu_liturgijaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Valandu_liturgijaApp.swift
+//  Valandu liturgija
+//
+//  Created by Rutenis Piksrys on 2021-02-09.
+//
+
+import SwiftUI
+
+@main
+struct Valandu_liturgijaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
